0d11323ca59ed16b89b2dd3296dc4b7210d4f326
Manit <manithirani@gmail.com>
2012-12-12 16:53:58 +0530

    Adding Embedvideo extension and WikiEditor

--- LocalSettings.php
+++ LocalSettings.php
@@ -140,3 +140,15 @@ $wgGroupPermissions['*']['createtalk'] = false;
 # End of automatically generated settings.
 # Add more configuration options below.
 
+require_once( "$IP/extensions/WikiEditor/WikiEditor.php" );
+#Enables use of WikiEditor by default but still allow users to disable it in preferences
+$wgDefaultUserOptions['usebetatoolbar'] = 1;
+$wgDefaultUserOptions['usebetatoolbar-cgd'] = 1;
+
+# Displays the Preview and Changes tabs
+$wgDefaultUserOptions['wikieditor-preview'] = 1;
+
+# Displays the Publish and Cancel buttons on the top right side
+$wgDefaultUserOptions['wikieditor-publish'] = 1;
+
+require_once("$IP/extensions/EmbedVideo/EmbedVideo.php");
